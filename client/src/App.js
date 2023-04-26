a7a32a885d5ce3990d0fba9fece003743ba02ba2
IT21016752 <it21016752@my.sliit.lk>
2023-04-26 17:26:49 +0530

    code changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddItem from './pages/ItemManagement/AddItem.js';
+import AddDeliveryInfo from './pages/DeliveryManagement/addDelInfo';
+import Header from './pages/Header.js';
+import AllItems from './pages/ItemManagement/AllItems.js';
+import DisplayItems from './pages/ItemManagement/DisplayItems';
+import AllDelInfo from './pages/DeliveryManagement/AlldelInfo';
+import AddPayInfo from './pages/PaymentManagement/AddPayInfo';
+import AllPayments from './pages/PaymentManagement/AllPayments';
+import Login from './pages/UserManagement/login';
+import Signup from './pages/UserManagement/signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Header />}>
+          <Route path="/" />
+          <Route path="/additems" element={<AddItem />} />
+          <Route path="/allitems" element={<AllItems />} />
+          <Route path="/AddDelInfo" element={<AddDeliveryInfo />} />
+          <Route path="/AllDelInfo" element={<AllDelInfo />} />
+          <Route path="/displayitems" element={<DisplayItems />} />
+          <Route path="/AddPayInfo" element={<AddPayInfo/>}/>
+          <Route path="/AllPayments" element={<AllPayments/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
+        </Route>
+      </Routes>
+    </Router>
+   
+    
   );
+  
 }
 
 export default App;
